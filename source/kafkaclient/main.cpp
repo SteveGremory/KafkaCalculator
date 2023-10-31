2587fce637dd520d994a6164de826ac9d48fc8ee
SteveGremory <stevegremory2006@gmail.com>
2023-10-31 20:27:53 +0530

    feat: added the cppkafka library and it's dependencies

--- source/kafkaclient/main.cpp
+++ source/kafkaclient/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <cppkafka/cppkafka.h>
 
 auto main() -> int {
-	std::cout << "Hello, Kafka." << std::endl;
+	// Create the config
+	cppkafka::Configuration config = {
+		{"metadata.broker.list", "127.0.0.1:9092"}};
+
+	// Create the producer
+	cppkafka::Producer producer(config);
+
+	// Produce a message!
+	std::string message = "hey there!";
+	producer.produce(
+		cppkafka::MessageBuilder("my_topic").partition(0).payload(message));
+	producer.flush();
 
 	return 0;
 }
